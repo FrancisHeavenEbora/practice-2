e9c070dd71deed664cd4ac4194b5c4db3020abd7
Francis Heaven Ebora <113603180+FrancisHeavenEbora@users.noreply.github.com>
2022-09-17 13:18:12 +0700

    Add files via upload

---
+++ practice 2.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter FIrst Number: "))
+
+num2 = int(input("Enter Second Number: "))
+
+
+
+if (num1 < num2):
+  
+  print(num1, "is less than", num2)
+
+elif (num1 > num2):
+
+  print(num1, "is greater than", num2)
+
+elif (num1 == num2):
+
+  print(num1, "and", num2, "is equal")
